cfa1c95287490cce995bf9bc3d65d5801eafa268
Mai Truong <94707519+maiimeo@users.noreply.github.com>
2025-02-03 23:38:58 -0600

    Update food.js

--- food.js
+++ food.js
@@ -1,6 +1,6 @@
 // Function to set the background with food icons
 function setFoodBackground() {
-    const foodBackgroundImage = 'url("https://i.imgur.com/eWsW51Z.jpeg")'; 
+    const foodBackgroundImage = 'url("https://i.imgur.com/FRz5L8e.jpeg")'; 
     const foodBackgroundSize = 'cover'; 
     const foodBackgroundPosition = 'center';
 
