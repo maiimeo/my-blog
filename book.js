04fc5523d5a530e4839131445a20748d098c7e0f
Mai Truong <94707519+maiimeo@users.noreply.github.com>
2025-02-16 23:27:21 -0600

    Update book.js

--- book.js
+++ book.js
@@ -1,14 +1,15 @@
 let currentPage = 0;
 const pages = document.querySelectorAll(".page");
+const totalPages = pages.length / 2;  // Divide by 2 because each page is in pairs (left-right)
 
 function changePage(direction) {
     currentPage += direction;
     if (currentPage < 0) {
-        currentPage = pages.length - 1;
-    } else if (currentPage >= pages.length) {
+        currentPage = totalPages - 1;
+    } else if (currentPage >= totalPages) {
         currentPage = 0;
     }
 
-    const bookContainer = document.querySelector(".book-container");
-    bookContainer.style.transform = `translateX(-${currentPage * 100}%)`;
+    const book = document.querySelector(".book");
+    book.style.transform = `translateX(-${currentPage * 50}%)`;
 }
