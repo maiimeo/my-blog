69ce472bbac60706f7fc50e250b9e3072633b272
Mai Truong <94707519+maiimeo@users.noreply.github.com>
2025-04-19 00:25:54 -0500

    Update autoplay-music.js

--- autoplay-music.js
+++ autoplay-music.js
@@ -1,5 +1,7 @@
 document.addEventListener('click', function () {
     const audio = document.getElementById('bg-music');
+
+    // Check if the audio is paused and then play it
     if (audio.paused) {
         audio.play();
     }
